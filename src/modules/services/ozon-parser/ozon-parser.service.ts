ea8c2ee2823466a24b29e2ef178628b75cb81e6a
Nikita Tyrkin <tyrkinn@pm.me>
2024-05-20 23:51:09 +0300

    feat: add no-sandbox option to browser

--- src/modules/services/ozon-parser/ozon-parser.service.ts
+++ src/modules/services/ozon-parser/ozon-parser.service.ts
@@ -37,7 +37,10 @@ export class OzonParser {
   }
 
   private async launchBrowser() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+    });
     const page = await browser.newPage();
     return { browser, page };
   }
